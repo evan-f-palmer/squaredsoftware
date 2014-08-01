d6685be8bf2699e2c9ccf15b1df351d31f289642
Evan Palmer <evan@Evans-Laptop>
2014-08-01 15:49:17 -0700

    Added script to create child project

---
+++ scripts/create-new-child.lua
@@ -0,0 +1,44 @@
+#!/usr/bin/lua
+local newName
+local outputDir
+local squaredSoftwareRepo          = "git@github.com:evan-f-palmer/squaredsoftware.git"
+local squaredSoftwareScriptsFolder = "squaredsoftware/scripts/"
+local tarName                      = "child.tar"
+
+local main
+local promptForInput
+local cloneSquaredSoftwareLib
+local untar
+local rename
+local promptForInput
+local cdToOutputDirThenExecute
+
+main = function()
+    outputDir = promptForInput("ENTER the output directory  : ")
+    assert(os.execute("mkdir -p " .. outputDir))
+    newName   = promptForInput("ENTER the new projects name : ")
+
+    cloneSquaredSoftwareLib()
+    untar()
+    rename()
+end
+
+cloneSquaredSoftwareLib = function()
+    cdToOutputDirThenExecute("git clone " .. squaredSoftwareRepo)
+end
+
+untar = function()
+    cdToOutputDirThenExecute("cp " .. squaredSoftwareFolder .. tarName .. " .") 
+    cdToOutputDirThenExecute("tar -zxf " .. tarName)
+    cdToOutputDirThenExecute("rm " .. tarName)
+end
+
+rename = function()
+    cdToOutputDirThenExecute("mv child " .. newName)
+end
+
+cdToOutputDirThenExecute = function(xCmd)
+    os.execute("cd " .. outputDir .. "; " .. xCmd)
+end
+
+main()
