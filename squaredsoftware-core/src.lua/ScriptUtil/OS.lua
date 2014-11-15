8a96b0a204b8d5c341c8b4a68f28a9d1de445cd2
evan <evan@Evans-Laptop>
2014-11-15 00:46:30 -0800

    Update OS.lua

--- squaredsoftware-core/src.lua/ScriptUtil/OS.lua
+++ squaredsoftware-core/src.lua/ScriptUtil/OS.lua
@@ -1,13 +1,18 @@
 --local pathSeparator        = os.getenv("PATH"):sub(1,1)
 --local DEF_WIN32, DEF_LINUX = pathSeparator == "\\", pathSeparator == "/"
-local pathSeparator = "/"
-local DEF_WIN32, DEF_LINUX = false, true
+local System = luajava.bindClass("java.lang.System");
+
+local osName = System:getProperty("os.name"):lower()
+local pathSeparator = System:getProperty("file.separator")
+local DEF_LINUX = osName == "linux"
+local DEF_WIN32 = osName == "windows"
 local thisOS
 
 if DEF_LINUX then
   require "ScriptUtil/Linux"
   thisOS = Linux
 end
+
 if DEF_WIN32 then
   require "ScriptUtil/Windows"
   thisOS = Windows
@@ -19,4 +24,5 @@ OS_DATA = {
   pathSeparator = pathSeparator,
   DEF_WIN32     = DEF_WIN32,
   DEF_LINUX     = DEF_LINUX,
+  osName        = osName,
 }
