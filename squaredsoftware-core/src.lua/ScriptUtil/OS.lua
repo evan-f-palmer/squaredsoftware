00e8ae42cf5745ad937a8d3e195b13deb5b19bfb
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2014-11-15 19:49:13 -0800

    Modified OS again to handle possible future errors with finding an applicable OS.

--- squaredsoftware-core/src.lua/ScriptUtil/OS.lua
+++ squaredsoftware-core/src.lua/ScriptUtil/OS.lua
@@ -8,18 +8,21 @@ local os_name = sys:getProperty("os.name")
 
 DEF_LINUX = string.match(os_name, "Linux")
 DEF_WIN32 = not DEF_LINUX
-OS        = getOS()
 
+OS               = getOS()
+OS.name          = os_name
+OS.arch          = sys:getProperty("os.arch")
+OS.version       = sys:getProperty("os.version")
 OS.pathSeparator = sys:getProperty("path.separator")
 OS.fileSeparator = sys:getProperty("file.separator")
 OS.lineSeparator = sys:getProperty("line.separator")
-OS.arch          = sys:getProperty("os.arch")
-OS.version       = sys:getProperty("os.version")
 OS.username      = sys:getProperty("user.name")      -- User's account name
 OS.userhome      = sys:getProperty("user.home")      -- User's home directory
 OS.userdir       = sys:getProperty("user.dir")       -- User's current working directory
 
 function getOS()
-  if DEF_LINUX then require "Linux";   return Linux;   end
-  if DEF_WIN32 then require "Windows"; return Windows; end
+  if      DEF_LINUX then require "Linux";   return Linux;
+  elseif  DEF_WIN32 then require "Windows"; return Windows; 
+  else                                      return {};
+  end
 end 
